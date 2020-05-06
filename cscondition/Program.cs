89c9495087a0008237c5e42f9f1dfc215c57ef71
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-06 16:01:02 +0900

    if else문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -21,6 +21,25 @@ namespace cscondition
             {
                 Console.WriteLine("홀수 입니다!");
             }
+
+            //현재 시간 구하기
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            if(DateTime.Now.Hour<12)
+            {
+                Console.WriteLine("오전입니다");
+            }
+            else
+            {
+                Console.WriteLine("오후입니다");
+            }
+
+
         }
     }
 }
