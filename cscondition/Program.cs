038625247a549c88c70672b124fe8ef6e18a19f9
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-06 16:12:30 +0900

    #2 switch문

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -13,6 +13,7 @@ namespace cscondition
             Console.WriteLine("숫자 입력: ");
             var input = int.Parse(Console.ReadLine());
 
+            /*
             if (input % 2 == 0)
             {
                 Console.WriteLine("짝수 입니다!");
@@ -20,6 +21,17 @@ namespace cscondition
             if(input %2==1)
             {
                 Console.WriteLine("홀수 입니다!");
+            }
+            */
+
+            switch(input%2)
+            {
+                case 0:
+                    Console.WriteLine("짝수 입니다!");
+                    break;
+                case 1:
+                    Console.WriteLine("홀수 입니다!");
+                    break;
             }
 
             //현재 시간 구하기
@@ -42,9 +54,35 @@ namespace cscondition
             {
                 Console.WriteLine("저녁 먹을 시간입니다");
             }
-            
 
-
+            Console.Write("이번 달은 몇 월인가요: ");
+            int month = int.Parse(Console.ReadLine());
+            switch(month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울입니다");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄입니다");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름입니다");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을입니다");
+                    break;
+                default:
+                    Console.WriteLine("어디 사니?");
+                    break;
+            }
         }
     }
 }
